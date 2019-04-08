5f88ae001b01e4b4dce4a93ab35f05b6e5f0b8f7
Spencer Lindemuth <lindemut@gmail.com>
2019-04-08 18:26:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,10 +5,13 @@ require 'yaml'
 
 def load_library
   # code goes here
+  YAML.load_file('emoticons.yml')
 end
 
 def get_japanese_emoticon
   # code goes here
+  load_library
+  binding.pry
 end
 
 def get_english_meaning
