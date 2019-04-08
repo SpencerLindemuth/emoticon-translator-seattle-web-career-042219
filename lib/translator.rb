f32de564c9436d50ef261bff09cd0716039d35e6
Spencer Lindemuth <lindemut@gmail.com>
2019-04-08 18:52:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(path)
   new_hash = {"get_meaning" => {key: []}, "get_emoticon" => {}}
   library = YAML.load_file(path)
   library.each do |key, value|
-    new_hash["get_meaning"] = {key => value[1]}
+    new_hash["get_meaning"][:key] << value[1]
     new_hash["get_emoticon"] = {value[0] => value[1]}
     end
   new_hash
